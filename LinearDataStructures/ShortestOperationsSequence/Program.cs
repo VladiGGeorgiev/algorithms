57381e76b00ddbd30b4f61cfa6a1c71eea60f77b
Vladimir Georgiev <vggeorgiev@mail.bg>
2013-06-01 01:31:54 +0300

    Homework from Linear Data Structures

---
+++ LinearDataStructures/ShortestOperationsSequence/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShortestOperationsSequence
+{
+    class Program
+    {
+        static int n = 5;
+        static int m = 30;
+        static Queue<int[]> queue = new Queue<int[]>();
+        static Stack<int> current = new Stack<int>();
+
+        static void Main(string[] args)
+        {
+            Queue<int> operationsSequence = new Queue<int>();
+            
+            Tree tree = new Tree(n);
+            var realTree = CreateTree(tree, n, m);
+
+            DFS(realTree.Root);
+
+            var a = queue.OrderBy(x => x.Count());
+            int minPathLength = a.First().Count();
+            var minPaths = a.Where(x => x.Count() == minPathLength);
+
+            foreach (var path in minPaths)
+            {
+                var currentPath = path.Reverse();
+                foreach (var num in currentPath)
+                {
+                    Console.Write(num + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        private static void BFS(TreeNode node)
+        {
+            Queue<TreeNode> queue = new Queue<TreeNode>();
+            queue.Enqueue(node);
+            while (queue.Count > 0)
+            {
+                var currentNode = queue.Dequeue();
+
+                Console.WriteLine(currentNode.Value);
+                if (currentNode.Value == 16)
+                {
+                    break;
+                }
+
+                foreach (var child in currentNode.Children)
+                {
+                    queue.Enqueue(child);
+                }
+            }
+        }
+  
+        private static void DFS(TreeNode node)
+        {
+            current.Push(node.Value);
+            if (node.Value == m)
+            {
+                queue.Enqueue(current.ToArray());
+            }
+            foreach (var child in node.Children)
+            {
+                DFS(child);
+            }
+            current.Pop();
+        }
+  
+        private static Tree CreateTree(Tree tree, int n, int m)
+        {
+            int currentElement = n;
+
+            if (currentElement > m)
+            {
+                return tree;
+            }
+
+            Tree currentChild = new Tree(currentElement * 2);
+            if (currentElement * 2 <= m)
+            {
+                var currentTree = CreateTree(currentChild, currentElement * 2, m);
+                tree.Root.AddChild(currentTree);
+            }
+
+            currentChild = new Tree(currentElement + 2);
+            if (currentElement + 2 <= m )
+            {
+                var currentTree = CreateTree(currentChild, currentElement + 2, m);
+                tree.Root.AddChild(currentTree);
+            }
+
+            currentChild = new Tree(currentElement + 1);
+            if (currentElement + 1 <= m)
+            {
+                var currentTree = CreateTree(currentChild, currentElement + 1, m);
+                tree.Root.AddChild(currentTree);   
+            }
+
+            return tree;
+        }
+    }
+}
---
+++ LinearDataStructures/ShortestOperationsSequence/TreeNode.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShortestOperationsSequence
+{
+    class TreeNode
+    {
+        private int value;
+        private List<TreeNode> children;
+        public bool hasParrent { get; set; }
+
+        public List<TreeNode> Children
+        {
+            get
+            {
+                return this.children;
+            }
+        }
+
+        public TreeNode(int value)
+        {
+            this.value = value;
+            this.children = new List<TreeNode>();
+        }
+
+        public int Value
+        {
+            get
+            {
+                return this.value;
+            }
+
+            set
+            {
+                this.value = value;
+            }
+        }
+
+        public void AddChild(TreeNode child)
+        {
+            child.hasParrent = true;
+            this.children.Add(child);
+        }
+
+        public void AddChild(params Tree[] children)
+        {
+            foreach (var child in children)
+            {
+                this.children.Add(child.Root);
+            }
+        }
+
+        public TreeNode GetChild(int index)
+        {
+            return this.children[index];
+        }
+    }
+}
