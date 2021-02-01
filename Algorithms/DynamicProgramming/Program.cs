ab0e6fce9206025c3ec6d003d1b9d0d4f632675a
VGGeorgiev <vladiggeorgiev@gmail.com>
2021-02-01 12:52:38 +0200

    Added another variation of string construction problem with memoization

--- Algorithms/DynamicProgramming/Program.cs
+++ Algorithms/DynamicProgramming/Program.cs
@@ -7,30 +7,30 @@ namespace DynamicProgramming
     {
         static void Main(string[] args)
         {
-            //int number = 10;
-            //var fibonacci = new Fibonacci();
-            //Console.WriteLine("Recursive fibonacci of {0}: {1}", number, fibonacci.FibonacciRecursive(number));
-            //Console.WriteLine("Recursive fibonacci with Memoization of {0}: {1}", number, fibonacci.FibonacciMemoization(number));
+            int number = 10;
+            var fibonacci = new Fibonacci();
+            Console.WriteLine("Recursive fibonacci of {0}: {1}", number, fibonacci.FibonacciRecursive(number));
+            Console.WriteLine("Recursive fibonacci with Memoization of {0}: {1}", number, fibonacci.FibonacciMemoization(number));
 
             Console.WriteLine();
 
-            //int rows = 17, cols = 17;
-            //var gridTraveller = new GridTraveller();
-            //var gridPaths = gridTraveller.TravelGrid(rows, cols);
-            //Console.WriteLine("Grid traveller ({0}, {1}) paths: {2}", rows, cols, gridPaths);
-            //var memoizedGridPaths = gridTraveller.TravelGridWithMemoization(rows, cols);
-            //Console.WriteLine("Grid traveller ({0}, {1}) paths: {2}", rows, cols, memoizedGridPaths);
+            int rows = 17, cols = 17;
+            var gridTraveller = new GridTraveller();
+            var gridPaths = gridTraveller.TravelGrid(rows, cols);
+            Console.WriteLine("Grid traveller ({0}, {1}) paths: {2}", rows, cols, gridPaths);
+            var memoizedGridPaths = gridTraveller.TravelGridWithMemoization(rows, cols);
+            Console.WriteLine("Grid traveller ({0}, {1}) paths: {2}", rows, cols, memoizedGridPaths);
 
             Console.WriteLine();
 
-            //var sumFinder = new PossibleSumFinder();
-            //int sum = 7;
-            //int[] numbers = new[] { 5, 3, 4, 7 };
-            //Console.WriteLine("Sum: {0}; Nums: [5, 3, 4, 7]; Result: {1}", sum, sumFinder.Find(sum, numbers));
-            //Console.WriteLine("Sum: 275; Nums: [7, 14]; Result: {0}", sumFinder.Find(275, new int[] { 7, 14 }));
-            //Console.WriteLine("Sum: 275; Nums: [7, 14]; Result: {0}", sumFinder.FindWithEarlyReturn(275, new int[] { 7, 14 }));
-            //Console.WriteLine("Sum: 275; Nums: [7, 14]; Result: {0}", sumFinder.FindWithMemoization(275, new int[] { 7, 14 }));
-            //Console.WriteLine();
+            var sumFinder = new PossibleSumFinder();
+            int sum = 7;
+            int[] numbers = new[] { 5, 3, 4, 7 };
+            Console.WriteLine("Sum: {0}; Nums: [5, 3, 4, 7]; Result: {1}", sum, sumFinder.Find(sum, numbers));
+            Console.WriteLine("Sum: 275; Nums: [7, 14]; Result: {0}", sumFinder.Find(275, new int[] { 7, 14 }));
+            Console.WriteLine("Sum: 275; Nums: [7, 14]; Result: {0}", sumFinder.FindWithEarlyReturn(275, new int[] { 7, 14 }));
+            Console.WriteLine("Sum: 275; Nums: [7, 14]; Result: {0}", sumFinder.FindWithMemoization(275, new int[] { 7, 14 }));
+            Console.WriteLine();
 
             var combinationSumFinder = new CombinationSumFinder();
             var result = combinationSumFinder.Find(7, new int[] { 5, 3, 4, 7 });
@@ -39,10 +39,18 @@ namespace DynamicProgramming
             var bestSumResult = bestSumFinder.Find(7, new int[] { 5, 3, 4, 7 });
 
             var stringConstructor = new StringConstructor();
+            var canConstruct = stringConstructor.CanConstruct("skateboard", 
+            var stringConstructor = new StringConstructor();
             var canConstruct = stringConstructor.CanConstruct("skateboard", new string[] { "bo", "rd", "ate", "t", "ska", "sk", "boar" });
             var canConstruct2 = stringConstructor.CanConstruct("abcdef", new string[] { "ab", "abc", "cd", "def", "abcd" });
             var canConstruct3 = stringConstructor.CanConstruct("aaaaaaaaaaaaaaaaaaaaaaaaaw", new string[] { "a", "aa", "aaa", "aaaa", "aaaaa", "aaaaaa" });
             var canConstruct4 = stringConstructor.CanConstructWithMemoization("aaaaaaaaaaaaaaaaaaaaaaaaaw", new string[] { "a", "aa", "aaa", "aaaa", "aaaaa", "aaaaaa" });
+
+            var stringWaysConstructor = new StringWaysConstructor();
+            int waysNumber = stringWaysConstructor.FindWaysToConstruct("abcdef", new string[] { "ab", "abc", "cd", "def", "abcd" }); // 1
+            int waysNumber2 = stringWaysConstructor.FindWaysToConstruct("aaaaaaaaaaaaaaaaaaaaaaaaaw", new string[] { "a", "aa", "aaa", "aaaa", "aaaaa", "aaaaaa" }); // 0
+            int waysNumber3 = stringWaysConstructor.FindWaysToConstruct("enterapotentpot", new string[] { "a", "p", "ent", "enter", "ot", "o", "t" }); // 4
+            var waysNumber4 = stringWaysConstructor.FindWaysToConstruct("skateboard", new string[] { "bo", "rd", "ate", "t", "ska", "sk", "boar" }); // 0
         }
     }
 }
---
+++ Algorithms/DynamicProgramming/StringWaysConstructor.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DynamicProgramming
+{
+    public class StringWaysConstructor
+    {
+        private Dictionary<string, int> wordsMemo = new Dictionary<string, int>();
+
+        public int FindWaysToConstruct(string word, string[] syllables)
+        {
+            if (string.IsNullOrEmpty(word)) return 1;
+            if (wordsMemo.ContainsKey(word))
+            {
+                return wordsMemo.GetValueOrDefault(word);
+            }
+
+            int numberOfWays = 0;
+            foreach (var syllable in syllables)
+            {
+                if (word.StartsWith(syllable))
+                {
+                    int waysNumber = FindWaysToConstruct(word.Remove(0, syllable.Length), syllables);
+                    numberOfWays += waysNumber;
+                }
+            }
+
+            wordsMemo.Add(word, numberOfWays);
+            return numberOfWays;
+        }
+    }
+}
